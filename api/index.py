e1b511057a0758273cd159cc3fb7580e9fc9a6e5
Nolan Fox <nolanfox@nolans-mbp-2.mynetworksettings.com>
2026-01-27 17:35:17 -0500

    Fix Vercel handler type error

--- api/index.py
+++ api/index.py
@@ -1,13 +1,10 @@
 import sys
 import os
 
-# Set up paths
+# Add the parent directory to the path so we can find 'main.py'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-# Import the app
+# Import the app. Vercel will automatically detect 'app' and run it as ASGI.
 from main import app
-
-# Vercel needs this
-handler = app
